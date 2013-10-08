54fd0e48bfc4d576c0c8807e9e351d9161139d51
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-10-08 10:11:56 -0300

    Improving code

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Algorithms in JavaScript
 
-A collection of many Algorithm's implementations in pure JavaScript
+A collection of many Algorithm's & Data Structure's implementations in pure JavaScript
+
+## Checking for quality
+
+I'm trying to write the code as good as possible, so I'm using JSlint to ensure that I get it
+
+```
+$ jslint binarySearchTree/BST.js
+```
 
 ## Licence
 
--- binarySearchTree/BST.js
+++ binarySearchTree/BST.js
@@ -1,34 +1,34 @@
 // Binary Search Tree implementation
 //
-// @author Rodrigo Alves @ CIn/UFPE
+// @author Rodrigo Alves
 // http://en.wikipedia.org/wiki/Binary_search_tree
 
-function BST (value) {
+function BST(value) {
     "use strict";
 
-    this.left = null;
-    this.right = null;
+    this.leftChild = null;
+    this.rightChild = null;
     this.key = value || 0;
-};
+}
 
 BST.prototype.insert = function (value) {
     "use strict";
 
     var node = new BST(value);
 
-    if (this.key == 0) {
+    if (this.key === 0) {
         this.key = value;
     } else if (value < this.key) {
-        if (this.left == null) {
-            this.left = node;
+        if (this.leftChild === null) {
+            this.leftChild = node;
         } else {
-            this.left.insert(value);
+            this.leftChild.insert(value);
         }
     } else {
-        if (this.right == null) {
-            this.right = node;
+        if (this.rightChild === null) {
+            this.rightChild = node;
         } else {
-            this.right.insert(value);
+            this.rightChild.insert(value);
         }
     }
 
@@ -40,11 +40,11 @@ BST.prototype.deleteMin = function () {
 
     var r = new BST();
 
-    if (this.left == null) {
-        r = this.right;
+    if (this.leftChild === null) {
+        r = this.rightChild;
         return r;
     } else {
-        this.left = this.left.deleteMin();
+        this.leftChild = this.leftChild.deleteMin();
         return this;
     }
 };
@@ -54,29 +54,29 @@ BST.prototype.delete = function (value) {
     var r = new BST(),
         smaller = new BST();
 
-    if (this == null) {
+    if (this === null) {
         return null;
     } else if (this.key > value) {
-        this.left = this.left.delete(value);
+        this.leftChild = this.leftChild.delete(value);
         return this;
     } else if (this.key < value) {
-        this.right = this.right.delete(value);
+        this.rightChild = this.rightChild.delete(value);
         return this;
     } else { // two children nodes
 
-        if (this.left != null && this.right != null) {
-            smaller = this.left;
-            this.right.left = smaller;
-            r = this.right;
+        if (this.leftChild !== null && this.rightChild !== null) {
+            smaller = this.leftChild;
+            this.rightChild.leftChild = smaller;
+            r = this.rightChild;
             return r;
         }
 
-        if (this.left == null) {
-            r = this.right;
-        } else if (this.right == null) {
-            r = this.left;
+        if (this.leftChild === null) {
+            r = this.rightChild;
+        } else if (this.rightChild === null) {
+            r = this.leftChild;
         } else {
-            this.right = this.right.deleteMin();
+            this.rightChild = this.rightChild.deleteMin();
             this.key = value;
         }
 
@@ -87,29 +87,29 @@ BST.prototype.delete = function (value) {
 BST.preOrder = function (root) {
     "use strict";
 
-    if (root != null) {
+    if (root !== null) {
         console.log(root.key + " ");
-        BST.preOrder(root.left);
-        BST.preOrder(root.right);
+        BST.preOrder(root.leftChild);
+        BST.preOrder(root.rightChild);
     }
 };
 
 BST.inOrder = function (root) {
     "use strict";
 
-    if (root != null) {
-        BST.preOrder(root.left);
+    if (root !== null) {
+        BST.preOrder(root.leftChild);
         console.warn(root.key + " ");
-        BST.preOrder(root.right);
+        BST.preOrder(root.rightChild);
     }
 };
 
 BST.postOrder = function (root) {
     "use strict";
 
-    if (root != null) {
-        BST.preOrder(root.left);
-        BST.preOrder(root.right);
+    if (root !== null) {
+        BST.preOrder(root.leftChild);
+        BST.preOrder(root.rightChild);
         console.log(root.key + " ");
     }
 };
@@ -117,9 +117,8 @@ BST.postOrder = function (root) {
 function main() {
     "use strict";
 
-    var tree = new BST(10);
-
-    var inputs = [90, 12, 41, 3, 104];
+    var tree = new BST(10),
+        inputs = [90, 12, 41, 3, 104];
 
     inputs.forEach(function (number) {
         tree.insert(number);
@@ -130,6 +129,6 @@ function main() {
     tree.delete(12);
 
     BST.inOrder(tree);
-};
+}
 
 main();
